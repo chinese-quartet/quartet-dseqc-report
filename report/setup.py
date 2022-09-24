0a887be1e4ad890376ef6994c5c0bcfa52428343
YaqingLiu <yaqing.liu@outlook.com>
2022-09-24 21:43:38 +0800

    revert: multiqc version

--- report/setup.py
+++ report/setup.py
@@ -21,7 +21,7 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     install_requires = [
-        'multiqc==1.12',
+        'multiqc==1.11',
         'plotly==4.9.0',
         'pandas==1.2.4',
         'seaborn==0.11.2',
